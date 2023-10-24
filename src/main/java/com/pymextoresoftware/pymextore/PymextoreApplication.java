a5935f04c63a39c9b29a491861d0662a7df85370
Velonz15422 <santiago15422@gmail.com>
2023-10-24 07:54:57 -0500

    first commit

---
+++ src/main/java/com/pymextoresoftware/pymextore/PymextoreApplication.java
@@ -0,0 +1,13 @@
+package com.pymextoresoftware.pymextore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PymextoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PymextoreApplication.class, args);
+	}
+
+}
